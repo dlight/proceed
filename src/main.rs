7a40dc50fd19ebd3d9702f72b50b088ee6b72c6c
Elias Gabriel Amaral da Silva <tolkiendili@gmail.com>
2018-07-07 09:37:19 -0300

    Some experiments with a counter and fps
    Exact text positioning is awful, it keeps adding random 1-2px of
    padding, so the width is unpredictable even if one uses a monospace
    font.
    
    This was reported in the following issues:
    
    https://github.com/ggez/ggez/issues/422
    https://github.com/ggez/ggez/issues/423

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,110 @@
 extern crate ggez;
-use ggez::graphics::{DrawMode, Point2};
+
+use ggez::graphics::*;
 use ggez::*;
 
-struct MainState {
-    pos_x: f32,
+struct FpsBox {
+    font: Font,
+    text: Vec<Text>,
+    margin: u32,
 }
 
-impl MainState {
-    fn new(_ctx: &mut Context) -> GameResult<MainState> {
-        let s = MainState { pos_x: 0.0 };
-        Ok(s)
+impl FpsBox {
+    fn new(ctx: &mut Context) -> GameResult<Self> {
+        let font = Font::default_font()?;
+        let text = vec![Text::new(ctx, "tick: 0", &font)?];
+
+        Ok(Self {
+            font,
+            text,
+            margin: 5,
+        })
     }
-}
 
-impl event::EventHandler for MainState {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
-        self.pos_x = self.pos_x % 800.0 + 1.0;
+    fn update(&mut self, ctx: &mut Context, tick: u32) -> GameResult<()> {
+        let fps = timer::get_fps(ctx);
+
+        let fps_str = format!("tick {}\nfps {:.0}", tick, fps);
+
+        let wrap_limit = 500;
+
+        let (_, lines) = self.font.get_wrap(&fps_str, wrap_limit);
+
+        self.text = lines
+            .iter()
+            .map(|line| graphics::Text::new(ctx, &line, &self.font))
+            .collect::<GameResult<_>>()?;
+
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
-        graphics::clear(ctx);
-        graphics::circle(
-            ctx,
-            DrawMode::Fill,
-            Point2::new(self.pos_x, 380.0),
-            100.0,
-            2.0,
-        )?;
-        graphics::present(ctx);
+        for (n, line) in self.text.iter().enumerate() {
+            let fps_x = ctx.conf.window_mode.width - line.width() - self.margin;
+
+            let fps_y = (n as u32) * (self.font.get_height() as u32) + self.margin;
+
+            let pos = Point2::new(fps_x as f32, fps_y as f32);
+
+            line.draw(ctx, pos, 0.0)?;
+        }
+
         Ok(())
     }
 }
 
-pub fn main() {
-    let c = conf::Conf::new();
-    let ctx = &mut Context::load_from_conf("super_simple", "ggez", c).unwrap();
-    let state = &mut MainState::new(ctx).unwrap();
-    event::run(ctx, state).unwrap();
+struct GameState {
+    fps_box: FpsBox,
+    wait_time: f32,
+    tick: u32,
+}
+
+impl GameState {
+    fn new(ctx: &mut Context, wait_time: f32) -> GameResult<Self> {
+        Ok(Self {
+            fps_box: FpsBox::new(ctx)?,
+            wait_time,
+            tick: 0,
+        })
+    }
+}
+
+impl event::EventHandler for GameState {
+    fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
+        let mut update_fps = false;
+
+        while timer::check_update_time(ctx, (1.0 / self.wait_time) as u32) {
+            self.tick += 1;
+
+            update_fps = true;
+        }
+
+        if update_fps {
+            self.fps_box.update(ctx, self.tick)?;
+        }
+
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        clear(ctx);
+
+        self.fps_box.draw(ctx)?;
+
+        present(ctx);
+
+        timer::yield_now();
+
+        Ok(())
+    }
+}
+
+pub fn main() -> GameResult<()> {
+    let mut ctx = Context::load_from_conf("proceed", "Elias Amaral", conf::Conf::new())?;
+
+    let mut state = GameState::new(&mut ctx, 0.5)?;
+
+    event::run(&mut ctx, &mut state)?;
+
+    Ok(())
 }
